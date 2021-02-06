5062973ca0557977a9ba2223653d82bbf29a7a75
Vadim Filimonov <philimonovvadim@gmail.com>
2021-02-07 00:29:18 +0300

    Add some corrections to eslint

--- src/components/TaskAdd.test.jsx
+++ src/components/TaskAdd.test.jsx
@@ -1,10 +1,8 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import TaskAdd from "./TaskAdd";
+import React from 'react';
+import {render} from '@testing-library/react';
+import TaskAdd from './TaskAdd';
 
-test("TaskAdd 1", () => {
-  const { asFragment } = render(
-    <TaskAdd />
-  );
+test('TaskAdd 1', () => {
+  const {asFragment} = render(<TaskAdd />);
   expect(asFragment()).toMatchSnapshot();
 });
