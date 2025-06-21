70b24da592ff4564e449f7a38ecc2e0d5978ed60
Vadim Filimonov <filimonov@duck.com>
2025-06-21 19:58:43 +0300

    Add helpers for local storage

--- src/helpers/storage.js
+++ src/helpers/storage.js
@@ -7,3 +7,13 @@ export const setReduxStateToStorage = (state) => {
   const persistedState = JSON.stringify(state);
   localStorage.setItem('reduxState', persistedState);
 };
+
+const SAVED_LANGUAGE_KEY = 'savedLanguage';
+
+export const getCurrentLanguageFromStorage = () => {
+  return localStorage.getItem(SAVED_LANGUAGE_KEY);
+};
+
+export const setCurrentLanguageToStorage = (language) => {
+  localStorage.setItem(SAVED_LANGUAGE_KEY, language);
+};
